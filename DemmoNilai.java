a4521d29fc297bae9be42eb5988480d0220a8cb2
azrael777-hub <azraelnasyok@gmail.com>
2026-02-28 23:09:39 +0700

    DemoNilai.java

---
+++ DemmoNilai.java
@@ -0,0 +1,14 @@
+public class DemoNilai {
+    public static void main(String[] args) {
+
+        Nilai n = new Nilai();
+
+        n.nim = "231234567";
+        n.nama = "Julian";
+        n.nilaiAbsen = 80;
+        n.nilaiTugas = 85;
+        n.nilaiUTS = 75;
+        n.nilaiUAS = 90;
+        n.CetakNilai();
+    }
+}
