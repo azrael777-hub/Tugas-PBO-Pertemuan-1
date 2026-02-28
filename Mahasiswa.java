b5210ee7eda0dce3c4dda4f256e36f7cc147680f
azrael777-hub <azraelnasyok@gmail.com>
2026-02-28 23:07:35 +0700

    Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,14 @@
+public class Mahasiswa {
+
+    void membaca() {
+        System.out.println("Mahasiswa sedang membaca.");
+    }
+
+    void nyontek() {
+        System.out.println("Mahasiswa sedang nyontek.");
+    }
+
+    void modifikasi() {
+        System.out.println("Mahasiswa sedang memodifikasi tugas.");
+    }
+}
